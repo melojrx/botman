43d5a18f055f0dc725c0291e549254aed71d2f8e
melojrx <jrmeloafrf@gmail.com>
2022-06-07 23:01:19 -0300

    login no heroku

--- conn.php
+++ conn.php
@@ -21,14 +21,22 @@ class conexao extends mysqli{
         $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
         */
     }
+    /*
+    user --> b7a2de452118e5
 
-
+    password --> 774ca8b5
+    
+    host --> us-cdbr-east-05.cleardb.net
+    
+    banco-de-dados --> heroku_0c61de362cfd21e
+    
+    */
     public function db(): mysqli
     {
-        $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbname = "fisiculturista";
+        $this->servername = "us-cdbr-east-05.cleardb.net";
+        $this->username = "b7a2de452118e5";
+        $this->password = "774ca8b5";
+        $this->dbname = "heroku_0c61de362cfd21e";
 
         static $db;
         $db OR $db = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
