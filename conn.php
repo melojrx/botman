14411aa6a40e3b119ed9d0b8aa6bc8f7879468b2
Cyro Sabóia <cyrosaboia@gmail.com>
2022-06-09 18:34:56 -0300

    conexao funcionando

--- conn.php
+++ conn.php
@@ -1,33 +1,33 @@
 <?php
 
-class conexao extends mysqli {
+class conexao extends mysqli{
 
     private $servername;
     private $username;
     private $password;
     private $dbname;
+    private $port;
 
-    public function __construct(){}
+    //public $conn;
 
-    public function db(): mysqli
+    public function __construct()
     {
-        // Testes
-//        $this->servername = "us-cdbr-east-05.cleardb.net";
-//        $this->username = "b964733ad066d5";
-//        $this->password = "3b2de8d6";
-//        $this->dbname = "heroku_02d5d5e218a30e2";
-
-        // Denvolvimento
+/*        
         $this->servername = "localhost";
         $this->username = "root";
         $this->password = "";
-        $this->dbname = "fisiculturismo";
-        
-        // Producao
-//        $this->servername = "";
-//        $this->username = "";
-//        $this->password = "";
-//        $this->dbname = "";
+        $this->dbname = "fisiculturista";
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        */
+    }
+
+
+    public function db(): mysqli
+    {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "fisicultrismo";
 
         static $db;
         $db OR $db = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
