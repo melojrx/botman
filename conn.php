0e4a632e8aa5c7f41b52f1c6dd587345ea65b553
Cyro Sabóia <cyrosaboia@gmail.com>
2022-06-10 17:53:56 -0300

    Ajustando conexao do hostgator

--- conn.php
+++ conn.php
@@ -8,26 +8,19 @@ class conexao extends mysqli{
     private $dbname;
     private $port;
 
-    //public $conn;
 
     public function __construct()
     {
-/*        
-        $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbname = "fisiculturista";
-        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-        */
+
     }
 
 
     public function db(): mysqli
     {
         $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->dbname = "fisicultrismo";
+        $this->username = "medic880_admin";
+        $this->password = "medicAdmin";
+        $this->dbname = "medic880_fisiculturismo";
 
         static $db;
         $db OR $db = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
